42e514816d95880e92588b19b58290fd6ded7d09
Swedz <me@swedz.net>
2024-04-16 04:01:54 -0400

    Initial setup

---
+++ src/main/java/net/swedz/intothetwilight/IntoTheTwilight.java
@@ -0,0 +1,24 @@
+package net.swedz.intothetwilight;
+
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Mod(IntoTheTwilight.ID)
+public final class IntoTheTwilight
+{
+	public static final String ID = "intothetwilight";
+	
+	public static ResourceLocation id(String name)
+	{
+		return new ResourceLocation(ID, name);
+	}
+	
+	public static final Logger LOGGER = LoggerFactory.getLogger("Into the Twilight");
+	
+	public IntoTheTwilight(IEventBus modBus)
+	{
+	}
+}
