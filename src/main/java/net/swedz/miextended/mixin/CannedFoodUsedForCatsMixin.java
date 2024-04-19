27a889e7a4fa27d25492229131b11b0761107bee
Swedz <me@swedz.net>
2024-04-19 01:02:42 -0400

    Remove debug log

--- src/main/java/net/swedz/miextended/mixin/CannedFoodUsedForCatsMixin.java
+++ src/main/java/net/swedz/miextended/mixin/CannedFoodUsedForCatsMixin.java
@@ -3,7 +3,6 @@ package net.swedz.miextended.mixin;
 import net.minecraft.world.entity.animal.Cat;
 import net.minecraft.world.entity.animal.Ocelot;
 import net.minecraft.world.level.ItemLike;
-import net.swedz.miextended.MIExtended;
 import net.swedz.miextended.items.MIEItems;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -23,12 +22,10 @@ public class CannedFoodUsedForCatsMixin
 	)
 	private static void modifyTemptIngredient(Args args)
 	{
-		MIExtended.LOGGER.info("modifyTemptIngredient1: {}", ((ItemLike[]) args.get(0))[0].asItem().toString());
 		ItemLike[] original = args.get(0);
 		ItemLike[] extended = new ItemLike[original.length + 1];
 		System.arraycopy(original, 0, extended, 0, original.length);
 		extended[original.length] = MIEItems.CANNED_FOOD;
 		args.set(0, extended);
-		MIExtended.LOGGER.info("modifyTemptIngredient2: {}", ((ItemLike[]) args.get(0))[original.length].asItem().toString());
 	}
 }
