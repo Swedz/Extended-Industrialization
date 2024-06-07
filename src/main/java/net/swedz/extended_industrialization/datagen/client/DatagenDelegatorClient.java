d664e42d227c55cebb4516466fdd4cbcb49043a5
Swedz <me@swedz.net>
2024-06-07 06:02:38 -0400

    Clean up MI datagen hook stuff

--- src/main/java/net/swedz/extended_industrialization/datagen/client/DatagenDelegatorClient.java
+++ src/main/java/net/swedz/extended_industrialization/datagen/client/DatagenDelegatorClient.java
@@ -1,6 +1,5 @@
 package net.swedz.extended_industrialization.datagen.client;
 
-import aztech.modern_industrialization.datagen.texture.MISpriteSourceProvider;
 import net.minecraft.data.DataProvider;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 import net.swedz.extended_industrialization.EI;
@@ -8,9 +7,7 @@ import net.swedz.extended_industrialization.EIFluids;
 import net.swedz.extended_industrialization.datagen.client.provider.LanguageDatagenProvider;
 import net.swedz.extended_industrialization.datagen.client.provider.models.BlockModelsDatagenProvider;
 import net.swedz.extended_industrialization.datagen.client.provider.models.ItemModelsDatagenProvider;
-import net.swedz.tesseract.neoforge.datagen.client.mi.LanguageMIHookDatagenProvider;
-import net.swedz.tesseract.neoforge.datagen.client.mi.MachineCasingModelsMIHookDatagenProvider;
-import net.swedz.tesseract.neoforge.datagen.client.mi.TexturesMIHookDatagenProvider;
+import net.swedz.tesseract.neoforge.datagen.mi.MIDatagenHooks;
 
 import java.util.function.Function;
 
@@ -18,11 +15,11 @@ public final class DatagenDelegatorClient
 {
 	public static void configure(GatherDataEvent event)
 	{
-		add(event, (__) -> new MISpriteSourceProvider(event.getGenerator().getPackOutput(), event.getLookupProvider(), event.getExistingFileHelper()));
+		MIDatagenHooks.Client.includeMISprites(event);
 		
-		add(event, (e) -> new TexturesMIHookDatagenProvider(e, EI.ID, EIFluids.values()));
-		add(event, (e) -> new LanguageMIHookDatagenProvider(e, EI.ID));
-		add(event, (e) -> new MachineCasingModelsMIHookDatagenProvider(e, EI.ID));
+		MIDatagenHooks.Client.addTexturesHook(event, EI.ID, EIFluids.values());
+		MIDatagenHooks.Client.addLanguageHook(event, EI.ID);
+		MIDatagenHooks.Client.addMachineCasingModelsHook(event, EI.ID);
 		
 		add(event, BlockModelsDatagenProvider::new);
 		add(event, ItemModelsDatagenProvider::new);
