e5e9dc684d843b14da1e65fe054de4ad38451df0
Swedz <me@swedz.net>
2024-08-24 15:34:15 -0400

    Improve mixin compat

--- src/main/java/net/swedz/extended_industrialization/mixin/NanoArmorApplyDyeColorMixin.java
+++ src/main/java/net/swedz/extended_industrialization/mixin/NanoArmorApplyDyeColorMixin.java
@@ -1,5 +1,7 @@
 package net.swedz.extended_industrialization.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.ItemStack;
@@ -7,12 +9,11 @@ import net.minecraft.world.item.component.DyedItemColor;
 import net.swedz.extended_industrialization.EITags;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(DyedItemColor.class)
 public class NanoArmorApplyDyeColorMixin
 {
-	@Redirect(
+	@WrapOperation(
 			method = "applyDyes",
 			at = @At(
 					value = "INVOKE",
@@ -20,8 +21,9 @@ public class NanoArmorApplyDyeColorMixin
 			)
 	)
 	private static int getDyeColor(DyeColor dyeColor,
+								   Operation<Integer> original,
 								   @Local(argsOnly = true) ItemStack stack)
 	{
-		return stack.is(EITags.NANO_ARMOR) ? dyeColor.getTextColor() : dyeColor.getTextureDiffuseColor();
+		return stack.is(EITags.NANO_ARMOR) ? dyeColor.getTextColor() : original.call(dyeColor);
 	}
 }
