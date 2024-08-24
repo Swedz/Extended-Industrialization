1072dcb303323e720575604811a69be406dd8c75
Swedz <me@swedz.net>
2024-08-24 16:34:17 -0400

    Use better colors for dyes on nano armor

--- src/main/java/net/swedz/extended_industrialization/mixin/NanoArmorApplyDyeColorMixin.java
+++ src/main/java/net/swedz/extended_industrialization/mixin/NanoArmorApplyDyeColorMixin.java
@@ -6,6 +6,7 @@ import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.DyedItemColor;
+import net.swedz.extended_industrialization.EIArmorMaterials;
 import net.swedz.extended_industrialization.EITags;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -24,6 +25,6 @@ public class NanoArmorApplyDyeColorMixin
 								   Operation<Integer> original,
 								   @Local(argsOnly = true) ItemStack stack)
 	{
-		return stack.is(EITags.NANO_ARMOR) ? dyeColor.getTextColor() : original.call(dyeColor);
+		return stack.is(EITags.NANO_ARMOR) ? EIArmorMaterials.getNanoColor(dyeColor) : original.call(dyeColor);
 	}
 }
