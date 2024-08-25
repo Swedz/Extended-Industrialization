59a8af97b9520ebe792c5b67a5e3a1c195368db5
Swedz <me@swedz.net>
2024-08-25 00:08:40 -0400

    Nano suit night vision

--- src/main/java/net/swedz/extended_industrialization/item/NanoSuitArmorItem.java
+++ src/main/java/net/swedz/extended_industrialization/item/NanoSuitArmorItem.java
@@ -1,31 +1,73 @@
 package net.swedz.extended_industrialization.item;
 
 import net.minecraft.core.Holder;
-import net.minecraft.world.entity.Entity;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
+import net.swedz.tesseract.neoforge.item.ArmorTickHandler;
+import net.swedz.tesseract.neoforge.item.ArmorUnequippedHandler;
 
-public final class NanoSuitArmorItem extends ElectricArmorItem
+public final class NanoSuitArmorItem extends ElectricArmorItem implements ArmorTickHandler, ArmorUnequippedHandler
 {
+	private static final long NIGHT_VISION_ENERGY = 4;
+	
 	public NanoSuitArmorItem(Holder<ArmorMaterial> material, Type type, Properties properties, long energyCapacity)
 	{
 		super(material, type, properties, energyCapacity);
 	}
 	
-	@Override
-	public void inventoryTick(ItemStack stack, Level level, Entity entity, int slot, boolean selected)
+	private boolean hasNightVision(LivingEntity entity)
 	{
-		if(level.isClientSide())
+		return entity.hasEffect(MobEffects.NIGHT_VISION) &&
+			   entity.getEffect(MobEffects.NIGHT_VISION).isInfiniteDuration();
+	}
+	
+	private void maybeAddNightVision(LivingEntity entity)
+	{
+		if(!this.hasNightVision(entity))
+		{
+			entity.addEffect(new MobEffectInstance(MobEffects.NIGHT_VISION, MobEffectInstance.INFINITE_DURATION, 0, false, false, false));
+		}
+	}
+	
+	private void maybeRemoveNightVision(LivingEntity entity)
+	{
+		if(this.hasNightVision(entity))
+		{
+			entity.removeEffect(MobEffects.NIGHT_VISION);
+		}
+	}
+	
+	@Override
+	public void armorTick(LivingEntity entity, EquipmentSlot slot, ItemStack stack)
+	{
+		if(entity.level().isClientSide())
 		{
 			return;
 		}
-		
-		if(type == Type.HELMET && entity instanceof Player player && stack == player.getItemBySlot(EquipmentSlot.HEAD))
+		if(slot == EquipmentSlot.HEAD && type == Type.HELMET)
 		{
-			// TODO night vision
+			if(this.getStoredEnergy(stack) >= NIGHT_VISION_ENERGY)
+			{
+				this.tryUseEnergy(stack, NIGHT_VISION_ENERGY);
+				this.maybeAddNightVision(entity);
+			}
+			else
+			{
+				this.maybeRemoveNightVision(entity);
+			}
+		}
+	}
+	
+	@Override
+	public void onUnequipArmor(LivingEntity entity, EquipmentSlot slot, ItemStack fromStack, ItemStack toStack)
+	{
+		if(!ItemStack.isSameItem(fromStack, toStack))
+		{
+			this.maybeRemoveNightVision(entity);
 		}
 	}
 }
