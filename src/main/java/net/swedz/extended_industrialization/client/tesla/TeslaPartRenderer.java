27cc18d9815145c653db28a469f43c0185002d74
Swedz <me@swedz.net>
2024-10-05 03:58:36 -0400

    Start work on rendering plasma on the top-load of the tower

--- src/main/java/net/swedz/extended_industrialization/client/tesla/TeslaPartRenderer.java
+++ src/main/java/net/swedz/extended_industrialization/client/tesla/TeslaPartRenderer.java
@@ -2,11 +2,23 @@ package net.swedz.extended_industrialization.client.tesla;
 
 import aztech.modern_industrialization.machines.MachineBlockEntity;
 import aztech.modern_industrialization.util.RenderHelper;
+import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.blaze3d.vertex.VertexFormat;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderStateShard;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.shapes.BooleanOp;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import net.swedz.extended_industrialization.EI;
 import net.swedz.extended_industrialization.EIComponents;
 import net.swedz.extended_industrialization.api.WorldPos;
@@ -22,6 +34,8 @@ import team.lodestar.lodestone.systems.rendering.trail.TrailPointBuilder;
 
 import java.util.List;
 import java.util.Optional;
+
+import static net.minecraft.client.renderer.RenderStateShard.*;
 
 final class TeslaPartRenderer
 {
@@ -50,7 +64,7 @@ final class TeslaPartRenderer
 				player.getOffhandItem().has(EIComponents.SELECTED_TESLA_NETWORK) ? Optional.of(player.getOffhandItem().get(EIComponents.SELECTED_TESLA_NETWORK)) : Optional.empty();
 	}
 	
-	private static final LodestoneRenderType RENDER_LAYER = LodestoneRenderTypes.TRANSPARENT_TEXTURE.applyAndCache(RenderTypeToken.createToken(EI.id("textures/vfx/tesla_arc.png")));
+	private static final LodestoneRenderType TESLA_ARC = LodestoneRenderTypes.TRANSPARENT_TEXTURE.applyAndCache(RenderTypeToken.createToken(EI.id("textures/vfx/tesla_arc.png")));
 	
 	private static void renderArcs(MachineBlockEntity machine, float partialTick, PoseStack matrices, MultiBufferSource buffer, int light, int overlay)
 	{
@@ -58,7 +72,7 @@ final class TeslaPartRenderer
 		{
 			VFXBuilders.WorldVFXBuilder builder = VFXBuilders.createWorld();
 			builder.replaceBufferSource(RenderHandler.LATE_DELAYED_RENDER.getTarget())
-					.setRenderType(RENDER_LAYER)
+					.setRenderType(TESLA_ARC)
 					.setColorRaw(1f, 1f, 1f)
 					.setAlpha(0.9f);
 			
@@ -86,9 +100,162 @@ final class TeslaPartRenderer
 		}
 	}
 	
+	private static final VoxelShape TESLA_TOP_LOAD_SHAPE;
+	
+	static
+	{
+		VoxelShape shape = Shapes.empty();
+		
+		for(int y = -2; y <= 2; y++)
+		{
+			for(int x = -1; x <= 1; x++)
+			{
+				for(int z = -1; z <= 1; z++)
+				{
+					shape = Shapes.joinUnoptimized(shape, Shapes.create(AABB.unitCubeFromLowerCorner(new Vec3(x, y, z)).inflate(0.05)), BooleanOp.OR);
+				}
+			}
+		}
+		
+		for(int y = -1; y <= 1; y++)
+		{
+			for(int x = -2; x <= 2; x++)
+			{
+				for(int z = -1; z <= 1; z++)
+				{
+					shape = Shapes.joinUnoptimized(shape, Shapes.create(AABB.unitCubeFromLowerCorner(new Vec3(x, y, z)).inflate(0.05)), BooleanOp.OR);
+					if(x != z)
+					{
+						shape = Shapes.joinUnoptimized(shape, Shapes.create(AABB.unitCubeFromLowerCorner(new Vec3(z, y, x)).inflate(0.05)), BooleanOp.OR);
+					}
+				}
+			}
+		}
+		
+		TESLA_TOP_LOAD_SHAPE = shape;
+	}
+	
+	private static RenderType getPlasmaRenderType(float u, float v)
+	{
+		return RenderType.create(
+				"plasma", DefaultVertexFormat.NEW_ENTITY,
+				VertexFormat.Mode.QUADS, 1536,
+				false, true,
+				RenderType.CompositeState.builder()
+						.setShaderState(RENDERTYPE_ENERGY_SWIRL_SHADER)
+						.setTextureState(new RenderStateShard.TextureStateShard(EI.id("textures/vfx/plasma_overlay.png"), false, false))
+						.setTexturingState(new RenderStateShard.OffsetTexturingStateShard(u, v))
+						.setTransparencyState(TRANSLUCENT_TRANSPARENCY)
+						.setCullState(NO_CULL)
+						.setLightmapState(LIGHTMAP)
+						.setOverlayState(OVERLAY)
+						.createCompositeState(false)
+		);
+	}
+	
+	private static void renderPlasma(MachineBlockEntity machine, float partialTick, PoseStack matrices, MultiBufferSource buffer, int light, int overlay)
+	{
+		matrices.pushPose();
+		
+		float tick = Minecraft.getInstance().levelRenderer.getTicks() + partialTick;
+		float speed = 0.015f;
+		float u = Mth.cos(tick * speed) * 0.5f % 1f;
+		float v = tick * speed * 0.75f % 1f;
+		VertexConsumer vc = buffer.getBuffer(getPlasmaRenderType(u, v));
+		
+		for(AABB box : TESLA_TOP_LOAD_SHAPE.toAabbs())
+		{
+			for(Direction direction : Direction.values())
+			{
+				renderPlasmaAddVertexesFace(
+						matrices, light, overlay, vc, direction,
+						(float) box.minX, (float) box.minY, (float) box.minZ,
+						(float) box.maxX, (float) box.maxY, (float) box.maxZ,
+						0, 0
+				);
+			}
+		}
+		
+		/*LevelRenderer.renderVoxelShape(
+				matrices,
+				buffer.getBuffer(RenderType.lineStrip()),
+				TESLA_TOP_LOAD_SHAPE,
+				0, 0, 0,
+				0f, 0f, 1f, 1f, true
+		);*/
+		
+		matrices.popPose();
+	}
+	
+	private static void renderPlasmaAddVertexesFace(PoseStack matrices, int light, int overlay,
+													VertexConsumer vc,
+													Direction direction,
+													float x1, float y1, float z1,
+													float x2, float y2, float z2,
+													float u, float v)
+	{
+		// TODO calculate UV offsets based on the side - currently it scales wrong on taller/wider faces
+		float offsetV = 0.5f * 3f;
+		float offsetU = (offsetV / 2f);// * 3f;
+		switch (direction)
+		{
+			case DOWN:
+				renderPlasmaAddVertex(matrices, light, overlay, vc, x1, y1, z1, u, v);
+				renderPlasmaAddVertex(matrices, light, overlay, vc, x2, y1, z1, u + offsetU, v);
+				renderPlasmaAddVertex(matrices, light, overlay, vc, x2, y1, z2, u + offsetU, v + offsetV);
+				renderPlasmaAddVertex(matrices, light, overlay, vc, x1, y1, z2, u, v + offsetV);
+				break;
+			case UP:
+				renderPlasmaAddVertex(matrices, light, overlay, vc, x1, y2, z1, u, v);
+				renderPlasmaAddVertex(matrices, light, overlay, vc, x1, y2, z2, u + offsetU, v);
+				renderPlasmaAddVertex(matrices, light, overlay, vc, x2, y2, z2, u + offsetU, v + offsetV);
+				renderPlasmaAddVertex(matrices, light, overlay, vc, x2, y2, z1, u, v + offsetV);
+				break;
+			case NORTH:
+				renderPlasmaAddVertex(matrices, light, overlay, vc, x1, y1, z1, u, v);
+				renderPlasmaAddVertex(matrices, light, overlay, vc, x1, y2, z1, u + offsetU, v);
+				renderPlasmaAddVertex(matrices, light, overlay, vc, x2, y2, z1, u + offsetU, v + offsetV);
+				renderPlasmaAddVertex(matrices, light, overlay, vc, x2, y1, z1, u, v + offsetV);
+				break;
+			case SOUTH:
+				renderPlasmaAddVertex(matrices, light, overlay, vc, x1, y1, z2, u, v);
+				renderPlasmaAddVertex(matrices, light, overlay, vc, x2, y1, z2, u + offsetU, v);
+				renderPlasmaAddVertex(matrices, light, overlay, vc, x2, y2, z2, u + offsetU, v + offsetV);
+				renderPlasmaAddVertex(matrices, light, overlay, vc, x1, y2, z2, u, v + offsetV);
+				break;
+			case EAST:
+				renderPlasmaAddVertex(matrices, light, overlay, vc, x2, y1, z1, u, v);
+				renderPlasmaAddVertex(matrices, light, overlay, vc, x2, y2, z1, u + offsetU, v);
+				renderPlasmaAddVertex(matrices, light, overlay, vc, x2, y2, z2, u + offsetU, v + offsetV);
+				renderPlasmaAddVertex(matrices, light, overlay, vc, x2, y1, z2, u, v + offsetV);
+				break;
+			case WEST:
+				renderPlasmaAddVertex(matrices, light, overlay, vc, x1, y1, z1, u, v);
+				renderPlasmaAddVertex(matrices, light, overlay, vc, x1, y1, z2, u + offsetU, v);
+				renderPlasmaAddVertex(matrices, light, overlay, vc, x1, y2, z2, u + offsetU, v + offsetV);
+				renderPlasmaAddVertex(matrices, light, overlay, vc, x1, y2, z1, u, v + offsetV);
+				break;
+		}
+	}
+	
+	private static void renderPlasmaAddVertex(PoseStack matrices, int light, int overlay,
+											  VertexConsumer vc,
+											  float x, float y, float z,
+											  float u, float v)
+	{
+		PoseStack.Pose pose = matrices.last();
+		vc.addVertex(pose, x, y, z)
+				.setColor(1f, 1f, 1f, 0.5f)
+				.setLight(light)
+				.setNormal(pose, 0, 0, 0)
+				.setOverlay(overlay)
+				.setUv(u, v);
+	}
+	
 	static void render(MachineBlockEntity machine, float partialTick, PoseStack matrices, MultiBufferSource buffer, int light, int overlay)
 	{
 		renderHighlight(machine, partialTick, matrices, buffer, light, overlay);
 		renderArcs(machine, partialTick, matrices, buffer, light, overlay);
+		renderPlasma(machine, partialTick, matrices, buffer, light, overlay);
 	}
 }
