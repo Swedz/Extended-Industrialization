93cd41ed9ba89ff5bf2261f377a8021b970cefb9
Kazuhiko-Gushiken <132615999+Kazuhiko-Gushiken@users.noreply.github.com>
2024-10-14 23:30:02 -0700

    this too!

--- src/main/java/net/swedz/extended_industrialization/EIText.java
+++ src/main/java/net/swedz/extended_industrialization/EIText.java
@@ -96,8 +96,8 @@ public enum EIText implements MICompatibleTranslatableTextEnum
 	STEAM_CHAINSAW_1("- Press %s on still or flowing water to fill."),
 	STEAM_CHAINSAW_2("- Place fuel inside the chainsaw using %s."),
 	STEAM_CHAINSAW_3("- Toggle Silk Touch with %s + %s."),
-	UNIVERSAL_TRANSFORMER_FROM_TIER_INPUT("Casing for cable tier to convert from (LV by default)."),
-	UNIVERSAL_TRANSFORMER_TO_TIER_INPUT("Casing for cable tier to convert to (LV by default)."),
+	UNIVERSAL_TRANSFORMER_FROM_TIER_INPUT("Hull for cable tier to convert from (LV by default)."),
+	UNIVERSAL_TRANSFORMER_TO_TIER_INPUT("Hull for cable tier to convert to (LV by default)."),
 	WASTE_COLLECTOR_HELP("When placed underneath animals, manure will be collected.");
 	
 	private final String englishText;
