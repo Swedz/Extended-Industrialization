7789a5e464094b139fe729e79c09761f98d48073
Swedz <me@swedz.net>
2025-01-09 18:22:25 -0500

    Buff Cupronickel Coil batch size for LEF

--- src/main/java/net/swedz/extended_industrialization/datagen/server/provider/datamaps/DataMapDatagenProvider.java
+++ src/main/java/net/swedz/extended_industrialization/datagen/server/provider/datamaps/DataMapDatagenProvider.java
@@ -31,7 +31,7 @@ public final class DataMapDatagenProvider extends DataMapProvider
 		this.addFluidFertilizerPotency(EIFluids.COMPOSTED_MANURE, 25, 150);
 		this.addFluidFertilizerPotency(EIFluids.NPK_FERTILIZER, 10, 30);
 		
-		this.addLargeElectricFurnaceTier(MI.id("cupronickel_coil"), 8, 0.75f);
+		this.addLargeElectricFurnaceTier(MI.id("cupronickel_coil"), 16, 0.75f);
 		this.addLargeElectricFurnaceTier(MI.id("kanthal_coil"), 32, 0.75f);
 	}
 	
