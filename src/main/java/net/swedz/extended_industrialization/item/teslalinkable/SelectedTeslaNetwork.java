df38f5f86c55685b5a1a2806d4b97c97132f4329
Swedz <me@swedz.net>
2025-01-11 21:49:54 -0500

    Clean up codec internals

--- src/main/java/net/swedz/extended_industrialization/item/teslalinkable/SelectedTeslaNetwork.java
+++ src/main/java/net/swedz/extended_industrialization/item/teslalinkable/SelectedTeslaNetwork.java
@@ -4,19 +4,25 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.RegistryFriendlyByteBuf;
-import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.world.level.block.Block;
 import net.swedz.tesseract.neoforge.api.WorldPos;
+import net.swedz.tesseract.neoforge.helper.CodecHelper;
 
 public record SelectedTeslaNetwork(WorldPos key, Block block)
 {
 	public static final Codec<SelectedTeslaNetwork> CODEC = RecordCodecBuilder.create((instance) -> instance
 			.group(
 					WorldPos.CODEC.fieldOf("key").forGetter(SelectedTeslaNetwork::key),
-					BuiltInRegistries.BLOCK.byNameCodec().fieldOf("block").forGetter(SelectedTeslaNetwork::block)
+					CodecHelper.forRegistry(BuiltInRegistries.BLOCK).fieldOf("block").forGetter(SelectedTeslaNetwork::block)
 			)
 			.apply(instance, SelectedTeslaNetwork::new));
 	
-	public static final StreamCodec<RegistryFriendlyByteBuf, SelectedTeslaNetwork> STREAM_CODEC = ByteBufCodecs.fromCodecWithRegistries(CODEC);
+	public static final StreamCodec<RegistryFriendlyByteBuf, SelectedTeslaNetwork> STREAM_CODEC = StreamCodec.composite(
+			WorldPos.STREAM_CODEC,
+			SelectedTeslaNetwork::key,
+			CodecHelper.forRegistryStream(BuiltInRegistries.BLOCK),
+			SelectedTeslaNetwork::block,
+			SelectedTeslaNetwork::new
+	);
 }
