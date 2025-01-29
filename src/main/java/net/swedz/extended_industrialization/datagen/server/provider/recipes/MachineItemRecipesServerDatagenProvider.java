77c0b69c14e419a67ef0f99f065ed4d9871dba73
Swedz <me@swedz.net>
2025-01-29 03:49:31 -0500

    Make Lethal Tesla Coil blast immune (from explosions and bosses)

--- src/main/java/net/swedz/extended_industrialization/datagen/server/provider/recipes/MachineItemRecipesServerDatagenProvider.java
+++ src/main/java/net/swedz/extended_industrialization/datagen/server/provider/recipes/MachineItemRecipesServerDatagenProvider.java
@@ -620,7 +620,8 @@ public final class MachineItemRecipesServerDatagenProvider extends RecipesServer
 						.define('H', "modern_industrialization:advanced_machine_hull")
 						.define('B', "modern_industrialization:sodium_battery")
 						.define('C', "modern_industrialization:aluminum_cable")
-						.pattern("CLC")
+						.define('P', "modern_industrialization:tungsten_large_plate")
+						.pattern("PLP")
 						.pattern("CHC")
 						.pattern("EBE"),
 				true,
