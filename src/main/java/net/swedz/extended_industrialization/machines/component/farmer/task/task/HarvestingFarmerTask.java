32ac18833f0a02fdb2bab1dc517393781d8370e6
Swedz <me@swedz.net>
2025-04-02 22:02:34 -0400

    Fix "POI data mismatch: never registered at ..." log spam when harvesting trees with bee hives

--- src/main/java/net/swedz/extended_industrialization/machines/component/farmer/task/task/HarvestingFarmerTask.java
+++ src/main/java/net/swedz/extended_industrialization/machines/component/farmer/task/task/HarvestingFarmerTask.java
@@ -5,10 +5,16 @@ import aztech.modern_industrialization.thirdparty.fabrictransfer.api.item.ItemVa
 import aztech.modern_industrialization.thirdparty.fabrictransfer.api.transaction.Transaction;
 import com.google.common.collect.Maps;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.FullChunkStatus;
+import net.minecraft.world.entity.ai.village.poi.PoiSection;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.chunk.LevelChunk;
+import net.neoforged.neoforge.common.extensions.IBlockStateExtension;
 import net.swedz.extended_industrialization.EITags;
 import net.swedz.extended_industrialization.datamap.EnchantmentModule;
 import net.swedz.extended_industrialization.machines.blockentity.multiblock.farmer.ElectricFarmerBlockEntity;
@@ -139,13 +145,69 @@ public final class HarvestingFarmerTask extends FarmerTask
 			var pos = blockPositions.get(index);
 			var oldState = oldStates[index];
 			var newState = newStates[index];
-			level.markAndNotifyBlock(pos, level.getChunkAt(pos), oldState, newState, Block.UPDATE_ALL, Block.UPDATE_LIMIT);
+			this.markAndNotifyBlockWithoutOnBlockStateChange(pos, level.getChunkAt(pos), oldState, newState, Block.UPDATE_ALL, Block.UPDATE_LIMIT);
 		}
 		cropBlockEntry.updateState(newOriginState);
 		
 		handler.harvested(context);
 		
 		return true;
+	}
+	
+	/**
+	 * This is a bit of a weird one so I think it warrants some explanation.
+	 * <br><br>
+	 * In the method linked below under "See Also", {@link Level#onBlockStateChange(BlockPos, BlockState, BlockState)}
+	 * and {@link IBlockStateExtension#onBlockStateChange(LevelReader, BlockPos, BlockState)} are always called. On the
+	 * server level (which this is always called on the server) that method causes POIs to get removed if the block it
+	 * is set to is no longer the same type of POI. This is fine in most cases, but in our case we need to remove
+	 * blocks and then go through and notify adjacent blocks about this change <i>after</i> all of the blocks have been
+	 * removed. This is to avoid certain types of blocks (like vines or moss carpets) that may be included in the batch
+	 * remove getting popped off instead of being harvested. That all being said, on the second iteration, we cannot
+	 * just use the method linked below because it causes the POIs to get removed a second time resulting in log spam
+	 * (for example: <code>POI data mismatch: never registered at ...</code>, see {@link PoiSection#remove(BlockPos)}
+	 * for the source of this message).
+	 * <br><br>
+	 * That all being said, this is the same code as the method linked below without the aforementioned
+	 * <code>onBlockStateChange</code> calls.
+	 *
+	 * @see Level#markAndNotifyBlock(BlockPos, LevelChunk, BlockState, BlockState, int, int)
+	 */
+	private void markAndNotifyBlockWithoutOnBlockStateChange(BlockPos pos, LevelChunk chunk, BlockState oldState, BlockState newState, int updateFlag, int updateLimit)
+	{
+		Block block = newState.getBlock();
+		BlockState currentState = level.getBlockState(pos);
+		if(currentState == newState)
+		{
+			if(oldState != currentState)
+			{
+				level.setBlocksDirty(pos, oldState, currentState);
+			}
+			
+			if((updateFlag & 2) != 0
+			   && (!level.isClientSide() || (updateFlag & 4) == 0)
+			   && (level.isClientSide() || chunk.getFullStatus() != null && chunk.getFullStatus().isOrAfter(FullChunkStatus.BLOCK_TICKING)))
+			{
+				level.sendBlockUpdated(pos, oldState, newState, updateFlag);
+			}
+			
+			if((updateFlag & 1) != 0)
+			{
+				level.blockUpdated(pos, oldState.getBlock());
+				if(!level.isClientSide() && newState.hasAnalogOutputSignal())
+				{
+					level.updateNeighbourForOutputSignal(pos, block);
+				}
+			}
+			
+			if((updateFlag & 16) == 0 && updateLimit > 0)
+			{
+				int flags = updateFlag & -34;
+				oldState.updateIndirectNeighbourShapes(level, pos, flags, updateLimit - 1);
+				newState.updateNeighbourShapes(level, pos, flags, updateLimit - 1);
+				newState.updateIndirectNeighbourShapes(level, pos, flags, updateLimit - 1);
+			}
+		}
 	}
 	
 	private Optional<EnchantmentModule> getActiveEnchantment()
