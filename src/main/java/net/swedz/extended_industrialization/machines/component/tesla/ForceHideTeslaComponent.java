c52f5a0b04c8d6991473e0b03b242b6c292e6dea
Swedz <me@swedz.net>
2025-08-12 19:22:15 -0400

    Add NBT tag for force hiding the tesla animation for the tesla particle generator

---
+++ src/main/java/net/swedz/extended_industrialization/machines/component/tesla/ForceHideTeslaComponent.java
@@ -0,0 +1,30 @@
+package net.swedz.extended_industrialization.machines.component.tesla;
+
+import aztech.modern_industrialization.machines.IComponent;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
+
+public final class ForceHideTeslaComponent implements IComponent
+{
+	private boolean forceHide;
+	
+	public boolean isHidden()
+	{
+		return forceHide;
+	}
+	
+	@Override
+	public void writeNbt(CompoundTag tag, HolderLookup.Provider registries)
+	{
+		if(forceHide)
+		{
+			tag.putBoolean("force_hide_tesla", forceHide);
+		}
+	}
+	
+	@Override
+	public void readNbt(CompoundTag tag, HolderLookup.Provider registries, boolean isUpgradingMachine)
+	{
+		forceHide = tag.getBoolean("force_hide_tesla");
+	}
+}
