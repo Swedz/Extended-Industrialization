af80e7dbe58ca014d55339abc1b4ffe205ad6822
Swedz <me@swedz.net>
2025-10-28 19:32:59 -0400

    Include fully charged items in creative tab (#254)

--- src/main/java/net/swedz/extended_industrialization/item/ElectricToolItem.java
+++ src/main/java/net/swedz/extended_industrialization/item/ElectricToolItem.java
@@ -786,7 +786,7 @@ public class ElectricToolItem extends Item implements DynamicToolItem, ISimpleEn
 	@Override
 	public long getEnergyCapacity(ItemStack stack)
 	{
-		return toolType.energyCapacity();
+		return quantum ? 0 : toolType.energyCapacity();
 	}
 	
 	@Override
