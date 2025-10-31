4e2010cda517ea7f17e5f061a0c15c73a7b82175
Swedz <me@swedz.net>
2025-10-30 23:17:21 -0400

    Better support for different types of crops (#257)

---
+++ src/main/java/net/swedz/extended_industrialization/machines/component/farmer/harvesting/harvestable/BushBlockHarvestable.java
@@ -0,0 +1,88 @@
+package net.swedz.extended_industrialization.machines.component.farmer.harvesting.harvestable;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.level.block.BushBlock;
+import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.block.NetherWartBlock;
+import net.minecraft.world.level.block.SweetBerryBushBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.swedz.extended_industrialization.machines.component.farmer.harvesting.HarvestingContext;
+import net.swedz.extended_industrialization.machines.component.farmer.harvesting.LootTableHarvestableBehavior;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * <p>This covers {@link CropBlock}, {@link NetherWartBlock}, {@link SweetBerryBushBlock}, or any other block that
+ * inherits {@link BushBlock} and has an {@link IntegerProperty} with the name <code>age</code>.</p>
+ */
+public final class BushBlockHarvestable implements LootTableHarvestableBehavior
+{
+	private static final Map<BlockState, IntegerProperty> AGEABLE_BLOCKS;
+	
+	static
+	{
+		// Cache the valid blocks so we dont have to search through all the properties of each block every time
+		Map<BlockState, IntegerProperty> ageableBlocks = Maps.newHashMap();
+		for(var block : BuiltInRegistries.BLOCK)
+		{
+			if(block instanceof BushBlock)
+			{
+				var defaultState = block.defaultBlockState();
+				// Check that this block has the age property
+				var ageProperty = getNamedIntegerProperty(defaultState, "age");
+				if(ageProperty != null)
+				{
+					// Ignore blocks with a "stage" property, like the mangrove propagule (or other potential weird modded saplings)
+					if(getNamedIntegerProperty(defaultState, "stage") != null)
+					{
+						continue;
+					}
+					for(var state : block.getStateDefinition().getPossibleStates())
+					{
+						ageableBlocks.put(state, ageProperty);
+					}
+				}
+			}
+		}
+		AGEABLE_BLOCKS = Collections.unmodifiableMap(ageableBlocks);
+	}
+	
+	private static IntegerProperty getNamedIntegerProperty(BlockState state, String name)
+	{
+		for(var property : state.getProperties())
+		{
+			if(property.getName().equals(name) && property instanceof IntegerProperty ageProperty)
+			{
+				return ageProperty;
+			}
+		}
+		return null;
+	}
+	
+	@Override
+	public boolean matches(HarvestingContext context)
+	{
+		return context.state().getBlock() instanceof BushBlock &&
+			   AGEABLE_BLOCKS.containsKey(context.state());
+	}
+	
+	@Override
+	public boolean isFullyGrown(HarvestingContext context)
+	{
+		var property = AGEABLE_BLOCKS.get(context.state());
+		return property != null &&
+			   context.state().getValue(property) == property.max;
+	}
+	
+	@Override
+	public List<BlockPos> getBlocks(HarvestingContext context)
+	{
+		return Lists.newArrayList(context.pos());
+	}
+}
