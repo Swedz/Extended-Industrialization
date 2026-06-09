8d0554c3ae2d96ce7f94283b953cd9925a7c4689
Swedz <me@swedz.net>
2026-06-08 20:34:22 -0400

    Fix #141: Tesla plasma texture scales incorrectly due to block atlas size

--- src/main/java/net/swedz/extended_industrialization/client/shader/TeslaPlasmaTextureStateShard.java
+++ src/main/java/net/swedz/extended_industrialization/client/shader/TeslaPlasmaTextureStateShard.java
@@ -1,16 +1,18 @@
 package net.swedz.extended_industrialization.client.shader;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.renderer.RenderStateShard;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.inventory.InventoryMenu;
 import net.swedz.extended_industrialization.EIClientShaders;
 
 public final class TeslaPlasmaTextureStateShard extends RenderStateShard.TextureStateShard
 {
 	private final float scale, speed;
 	
-	public TeslaPlasmaTextureStateShard(ResourceLocation texture, float scale, float speed)
+	public TeslaPlasmaTextureStateShard(ResourceLocation plasmaTexture, float scale, float speed)
 	{
-		super(texture, false, false);
+		super(plasmaTexture, false, false);
 		this.scale = scale;
 		this.speed = speed;
 	}
@@ -19,6 +21,8 @@ public final class TeslaPlasmaTextureStateShard extends RenderStateShard.Texture
 	public void setupRenderState()
 	{
 		super.setupRenderState();
+		
+		RenderSystem.setShaderTexture(3, InventoryMenu.BLOCK_ATLAS);
 		
 		EIClientShaders.teslaPlasma().getUniform("PlasmaScale").set(scale);
 		EIClientShaders.teslaPlasma().getUniform("PlasmaSpeed").set(speed);
